f19ec63eb8c30c50b476d287b00a517ba59ceb43
Kat Sauma <katsauma@gmail.com>
2021-03-22 18:53:21 -0400

    added second part of test with second set of expectations

--- tests/name.test.js
+++ tests/name.test.js
@@ -6,5 +6,9 @@ describe('name of object', () => {
         const spot = { name: 'spot', age: 5, weight: '20 lbs' };
 
         expect(getName(spot)).toEqual('spot');
+        
+        const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+        
+        expect(getName(character)).toEqual('Aang');
     });
 });
